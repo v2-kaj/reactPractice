2ebd5c2e166476048aa931b25d12b0fa2e4f11eb
v2-kaj <madalitsomuva@gmail.com>
2021-07-17 08:16:08 +0200

    filling x

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -5,18 +5,37 @@ import './index.css';
 class Square extends React.Component {
     render() {
       return (
-        <button className="square" onClick={()=>{alert(`${this.props.value} Clicked`)}}>
-            {this.props.value}
+        <button 
+          className="square" 
+          onClick={()=>this.props.onClick()}
+        >  
+          {this.props.value}
         </button>
       );
     }
   }
   
   class Board extends React.Component {
-      
-    renderSquare(i) {
-      return <Square value={i}/>;
+    constructor(props){
+      super(props);
+      this.state={
+        squares:Array(9).fill(null)
+       
+      }
     }
+    handleClick(i){
+      const squares=this.state.squares.slice()
+      squares[i]='X'
+      this.setState({ squares:squares })
+    }
+
+    renderSquare(i) {
+      return (
+        <Square 
+          value={this.state.squares[i]}
+          onClick={()=>{this.handleClick(i)}}
+        />
+      )}
   
     render() {
       const status = 'Next player: X';
